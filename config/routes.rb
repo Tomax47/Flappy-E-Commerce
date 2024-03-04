a327c7d30a3e7c210e916c01e6010214a616f469
root <tammam744@gmail.com>
2024-03-04 20:16:53 +0300

    Tailwind setupt 'correction/completion' & devise admin dashboard
    1- Set up the configurations for Tailwind scss for the project
    2- Finished up the authorization/authentication for the admin
    3- created the admin dashboard, and did some styling 'tested Tailwind integration status'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   
-  get 'home/index'
-  root "home#index"
+  devise_for :admins
+
+  root 'home#index'
+
+  authenticated :admin_user do 
+    root to: 'admin#index', as: :admin_root
+  end
+
+  get 'admin' => 'admin#index'
 end
